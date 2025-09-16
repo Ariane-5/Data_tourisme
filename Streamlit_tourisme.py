bec42d83ac333a075e20aec3146d5403306090a2
Ariane-5 <144696213+Ariane-5@users.noreply.github.com>
2025-09-16 22:38:03 +0200

    Update Streamlit_tourisme.py

--- Streamlit_tourisme.py
+++ Streamlit_tourisme.py
@@ -79,7 +79,7 @@ with col_dep :
 
   
 if type_lieux != "(tous)" :
-  select = df[(df['Categories_de_POI'].str.contains(type_lieux))]
+  select = df[(df['Categories_de_POI'].str.contains(type_lieux, na=False))]
 if type_lieux == "(tous)" :
   select = df
 
